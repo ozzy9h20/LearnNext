d6246bf25dd8510784d672a417dc043adc4786a5
Ozzy9h20 <mochiaria97@gmail.com>
2021-08-03 11:53:53 +0700

    3. Next Head & Layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
 export default function Home() {
   return (
-    <div>
+    <Layout>
+      <Head>
+        <title>DJ Events</title>
+        <meta name='description' content='dj events' />
+      </Head>
+
       <h1>Home</h1>
-    </div>
+    </Layout>
   )
 }
