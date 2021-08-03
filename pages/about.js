b3b702b8206e84f269df36131f0d424d0410d4e0
Ozzy9h20 <mochiaria97@gmail.com>
2021-08-03 11:36:41 +0700

    2. Next Link

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,17 @@
+import Link from 'next/link';
+
 export default function About() {
   return (
     <div>
       <h1>About</h1>
       <p>This is just another app to find the location of some musical events</p>
       <p>Version: 1.0.0</p>
+
+      <Link href="/">
+        <a>
+          Back to Home
+        </a>
+      </Link>
     </div>
   )
 }
