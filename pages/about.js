265c2ef24a585ea5fbc93b9ee3e0aa53ba71b207
Ozzy9h20 <mochiaria97@gmail.com>
2021-08-03 11:28:05 +0700

    1. Pages Routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is just another app to find the location of some musical events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
