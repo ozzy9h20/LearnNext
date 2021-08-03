b2749b61c322400fe8e82094515dd1172be395a0
Ozzy9h20 <mochiaria97@gmail.com>
2021-08-03 11:40:02 +0700

    Clearing next link

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-
-      <Link href="/about">
-        <a>
-          About
-        </a>
-      </Link>
     </div>
   )
 }
