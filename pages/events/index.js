85cc4b485bad88ead8563ff504c6a601635e011c
ozzy9h20 <mochiaria97@gmail.com>
2021-08-05 17:34:48 +0700

    4.2. Search Component & add qs

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,11 +2,11 @@ import Layout from '@/components/Layout';
 import EventItem from '@/components/EventItem';
 import { API_URL } from '@/config/index';
 
-export default function Home({ events }) {
+export default function EventsPage({ events }) {
   // console.log(events)
   return (
     <Layout>
-      <h1>Upcoming Events</h1>
+      <h1>Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
       
       {events.map((evt) => (
@@ -17,7 +17,7 @@ export default function Home({ events }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events`);
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
   const events = await res.json();
 
   return {
