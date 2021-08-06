591e84508b415fa71937de1859aaf8f9bb27ae58
ozzy9h20 <mochiaria97@gmail.com>
2021-08-06 08:48:54 +0700

    Delete Events

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,14 +1,31 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import styles from '@/styles/Event.module.css';
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function EventPage({ evt }) {
-  const deleteEvent = (e) => {
-    console.log('delete')
+  const router = useRouter();
+
+  const deleteEvent = async (e) => {
+    if (confirm('Are you sure?')) {
+      const res = await fetch(`${API_URL}/events/${evt.id}`, {
+        method: 'DELETE'
+      })
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+      }
+      else {
+        router.push('/events');
+      }
+    }
   }
   
 
