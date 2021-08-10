d0114d87a956faa94892e51042ef6a4417344a74
ozzy9h20 <mochiaria97@gmail.com>
2021-08-10 08:26:39 +0700

    [REFACTORING] Pagination Component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,8 @@
-import Link from 'next/link';
 import Layout from '@/components/Layout';
 import EventItem from '@/components/EventItem';
-import { API_URL } from '@/config/index';
-
-const PER_PAGE = 2;
-
-export default function EventsPage({ events, page, total }) {
-  const lastPage = Math.ceil(total / PER_PAGE);
-  
+import { API_URL, PER_PAGE } from '@/config/index';
+import Pagination from '@/components/Pagination';
+export default function EventsPage({ events, page, total }) {  
   return (
     <Layout>
       <h1>Events</h1>
@@ -17,17 +12,7 @@ export default function EventsPage({ events, page, total }) {
         <EventItem key={evt.id} evt={evt} />
       ))}
 
-      {page > 1 && (
-        <Link href={`events?page=${page - 1}`}>
-          <a className='btn-secondary'>Prev</a>
-        </Link>
-      )}
-
-      {page < lastPage && (
-        <Link href={`events?page=${page + 1}`}>
-          <a className='btn-secondary'>Next</a>
-        </Link>
-      )}
+      <Pagination page={page} total={total} />
     </Layout>
   )
 }
