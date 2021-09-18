02dc384dfdfee96de0f14f7f4398c4dbb1d872ed
Mochammad Endyarroziqin <mynameis_ozzy@yahoo.com>
2021-09-18 09:20:40 +0700

    Authenticated Edit, Delete & Image Upload

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -3,11 +3,31 @@ import DashboardEvent from "@/components/DashboardEvent";
 import { API_URL } from "@/config/index";
 import { parseCookies } from "@/helpers/index";
 import styles from '@/styles/Dashboard.module.css';
+import { useRouter } from 'next/router';
 
-export default function DashboardPage({ events }) {
-  const deleteEvent = (id) => {
-    console.log(id);
+export default function DashboardPage({ events, token }) {
+  const router = useRouter();
+
+  const deleteEvent = async (id) => {
+    if (confirm('Are you sure?')) {
+      const res = await fetch(`${API_URL}/events/${id}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+      }
+      else {
+        router.reload();
+      }
+    }
   }
+  
 
   return (
     <Layout title="User Dashboard">
@@ -37,7 +57,8 @@ export async function getServerSideProps({ req }) {
 
   return {
     props: {
-      events
+      events,
+      token
     }
   };
 }
--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -11,8 +11,9 @@ import { API_URL } from "@/config/index";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styles from "@/styles/Form.module.css";
+import { parseCookies } from '@/helpers/index';
 
-export default function AddEventPage({ evt }) {
+export default function AddEventPage({ evt, token }) {
   const [values, setValues] = useState({
     name: evt.name,
     performers: evt.performers,
@@ -41,11 +42,16 @@ export default function AddEventPage({ evt }) {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(values)
       });
   
       if (!res.ok) {
+        if (res.status === 403 || res.status === 401) {
+          toast.error('Unauthorized');
+          return;
+        }
         toast.error('Something Went Wrong');
       } else {
         const evt = await res.json();
@@ -170,21 +176,26 @@ export default function AddEventPage({ evt }) {
         show={showModal} 
         onClose={() => setShowModal(false)}
       >
-        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+        <ImageUpload 
+          evtId={evt.id} 
+          imageUploaded={imageUploaded} 
+          token={token}
+        />
       </Modal>
     </Layout>
   )
 }
 
 export async function getServerSideProps({ params: {id}, req }) {
+  const { token } = parseCookies(req);
+
   const res = await fetch(`${API_URL}/events/${id}`);
   const evt = await res.json();
 
-  console.log(req.headers.cookie);
-
   return {
     props: {
-      evt
+      evt,
+      token
     }
   };
 }
